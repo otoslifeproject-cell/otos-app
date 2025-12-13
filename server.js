4e23d42231b6dccf14f351e94d9569892f1d474f
otoslifeproject-cell <otoslifeproject@gmail.com>
2025-12-13 16:31:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.post("/feeder", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`OTOS Feeder running on port ${PORT}`);
+  console.log(`OTOS Feeder live on port ${PORT}`);
 });
