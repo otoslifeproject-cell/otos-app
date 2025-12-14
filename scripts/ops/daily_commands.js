dbd457ed9080b8c669efe29715b2a6578c75912a
otoslifeproject-cell <otoslifeproject@gmail.com>
2025-12-14 23:58:47 +0000

    Create daily_commands.js

---
+++ scripts/ops/daily_commands.js
@@ -0,0 +1,11 @@
+/**
+ * DAILY COMMAND SET
+ * Only commands allowed during daily operations
+ */
+
+export const DAILY_COMMANDS = [
+  "FEEDER_INGEST",
+  "SNAPSHOT_MEMORY",
+  "EXPORT_CANONICAL_DOC",
+  "RUN_HEARTBEAT"
+];
